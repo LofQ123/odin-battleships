f76bc2ace9431bb6b881e3b1b04550dc61ff55d1
LofQ123 <alexrus5495@gmail.com>
2024-11-19 00:40:58 +0300

    add Ship class with hit() and isSunk()

---
+++ src/ship.js
@@ -0,0 +1,16 @@
+export class Ship {
+  constructor(length) {
+    this.length = length;
+    this.timesHit = 0;
+    this.sunk = false;
+  }
+
+  hit() {
+    this.timesHit++
+    this.isSunk()
+  }
+
+  isSunk() {
+    if (this.timesHit === this.length) this.sunk = true;
+  }
+}
