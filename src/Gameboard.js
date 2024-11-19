a942be19e7bad82d32cfdfffb9528d8b8d481420
LofQ123 <alexrus5495@gmail.com>
2024-11-19 03:02:36 +0300

    add placeShip() function

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,12 +1,21 @@
+import { Ship } from "./ship";
+
 export class Gameboard {
   constructor(size) {
     this.board = [];
-    for (let x = 0; x < size; x++) {
+    for (let y = 0; y < size; y++) {
       let row = [];
-      for (let y = 0; y < size; y++) {
+      for (let x = 0; x < size; x++) {
         row.push(null);
       }
       this.board.push(row)
     }
+  }
+
+  placeShip(shipSize, coordinates) {
+    let ship = new Ship(shipSize)
+    coordinates.forEach(coord => {
+      this.board[coord[0]][coord[1]] = ship;
+    });
   }
 }
