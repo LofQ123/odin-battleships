594584ec4e6a8d3c414d0cb3b3d84174046e0c09
LofQ123 <alexrus5495@gmail.com>
2024-11-19 12:27:04 +0300

    add isFleetSunk() function to check if all ships were sunk

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -10,6 +10,7 @@ export class Gameboard {
       }
       this.board.push(row)
     }
+    this.fleet = [];
   }
 
   placeShip(shipSize, coordinates) {
@@ -17,11 +18,21 @@ export class Gameboard {
     coordinates.forEach(coord => {
       this.board[coord[0]][coord[1]] = ship;
     });
+    this.fleet.push(ship)
   }
 
   receiveAttack(coordinates) {
     const target = this.board[coordinates[0]][coordinates[1]];
     if (target === null) this.board[coordinates[0]][coordinates[1]] = 0;
     else this.board[coordinates[0]][coordinates[1]].hit();
+  }
+
+  isFleetSunk() {
+    if(!this.fleet.length) return null
+    let fleetSunk = true;
+    this.fleet.forEach(ship => {
+      if (!ship.sunk) fleetSunk = false;
+    })
+    return fleetSunk;
   }
 }
