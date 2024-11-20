c92e83cda990e30abf44d0c194cd012c69341604
LofQ123 <alexrus5495@gmail.com>
2024-11-20 15:34:05 +0300

    create css and js files for game screens

---
+++ src/dom_screen-placing.js
@@ -0,0 +1,12 @@
+const containerMain = document.getElementById("container_main");
+
+export function clearAll() {
+  containerMain.innerHTML = "";
+}
+
+export function displayScreen_mainMenu() {
+
+}
+
+
+
